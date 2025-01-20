a0e5da3c74b59f3a5d27135f4bf019b25b31dbf8
Florian Leuenberger <leuenberger.florian@gmx.ch>
2025-01-20 23:22:49 +0100

    style: Update booking page list style with numbers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ServicesComponent } from './components/services/services.component';
 import { BookingComponent } from './components/booking/booking.component';
-import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'booking', component: BookingComponent },
-  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' }
 ];
