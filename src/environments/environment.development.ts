8260de327c8b1cdc1670d10a6eccd25acd3c4793
Florian Leuenberger <leuenberger.florian@gmx.ch>
2025-01-24 15:51:14 +0100

    fix: remove CSP meta tag to allow mixed content

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   pocketbaseUrl: 'http://0.0.0.0:8090'
-  // pocketbaseUrl: 'http://0.0.0.0:8090'
 }; 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  pocketbaseUrl: 'https://217.160.252.61'
+  pocketbaseUrl: 'http://217.160.252.61:80'
 };
